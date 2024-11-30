4ce333433dc31efb9a7c2369a7fd01869a7983b1
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-12-01 00:03:18 +0100

    added parts

--- src/main/java/hu/nero/spring_security_web_login_thymeleaf/controller/WeatherController.java
+++ src/main/java/hu/nero/spring_security_web_login_thymeleaf/controller/WeatherController.java
@@ -1,12 +1,12 @@
 package hu.nero.spring_security_web_login_thymeleaf.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 @RequestMapping("/weather")
 public class WeatherController {
 
